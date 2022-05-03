3784c436f72fcef41d9bbc7ab4da34f20bb06332
ChristianSalon <80092102+ChristianSalon@users.noreply.github.com>
2022-05-03 21:47:19 +0200

    Payment webhook - no webhook signing

--- routes/payments.ts
+++ routes/payments.ts
@@ -77,18 +77,7 @@ router.post(
   "/succeeded",
   express.raw({ type: "application/json" }),
   (request, response) => {
-    const sig = request.headers["stripe-signature"];
-
-    let event;
-
-    try {
-      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-    } catch (err) {
-      response.status(400).send(`Webhook Error: ${err.message}`);
-      return;
-    }
-
-    console.log(event);
+    const event = request.body;
     let paymentIntent;
 
     switch (event.type) {
