3aba36dfd06f6de515f0cae5f05bd7383f314c56
ChristianSalon <80092102+ChristianSalon@users.noreply.github.com>
2022-05-14 17:04:22 +0200

    Expo notifications

--- routes/notifications.ts
+++ routes/notifications.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { NotificationToken } from "../types";
 const express = require("express");
 const mysql = require("mysql");
+const axios = require("axios");
 const router = express.Router();
 
 const connection = mysql.createConnection({
@@ -41,6 +42,89 @@ router.post("/", (req: Request, res: Response) => {
         res.status(500).send(error);
       } else {
         res.send("Token registered");
+      }
+    }
+  );
+});
+
+router.post("/user", (req: Request, res: Response) => {
+  const body: {
+    userId: string;
+    title: string;
+    body: string;
+  } = req.body;
+  let tokens;
+
+  connection.query(
+    `SELECT token FROM notification_tokens
+    WHERE userId = ?;`,
+    [body.userId],
+    async (error, results) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        tokens = results.map((result) => result.token);
+        const response = await axios.post(
+          "https://exp.host/--/api/v2/push/send",
+          {
+            to: tokens,
+            title: body.title,
+            body: body.body,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        if (response.status === 200) {
+          res.send("Notification sent");
+        } else {
+          res.status(500).send("Notification failed to send");
+        }
+      }
+    }
+  );
+});
+
+router.post("/teams", (req: Request, res: Response) => {
+  const body: {
+    teamIds: string[];
+    userId: string;
+    title: string;
+    body: string;
+  } = req.body;
+  let tokens;
+
+  connection.query(
+    `SELECT nt.token FROM notification_tokens AS nt
+    INNER JOIN teams_users AS tu ON nt.userId = tu.userId
+    WHERE tu.teamId IN (?) AND tu.userId != ?
+    GROUP BY tu.userId;`,
+    [body.teamIds, body.userId],
+    async (error, results) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        tokens = results.map((result) => result.token);
+        const response = await axios.post(
+          "https://exp.host/--/api/v2/push/send",
+          {
+            to: tokens,
+            title: body.title,
+            body: body.body,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        if (response.status === 200) {
+          res.send("Notifications sent");
+        } else {
+          res.status(500).send("Notifications failed to send");
+        }
       }
     }
   );
